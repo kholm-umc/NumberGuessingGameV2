0002532ccec3bdb28c3ba9f5edc2e2ae7086d272
kholm <Wired14004363!>
2020-10-14 06:43:12 -0500

    Additional error checking

--- main.py
+++ main.py
@@ -31,70 +31,90 @@ print("* Guess again if your guess is high or low")
 print("* You can enter the letter 'q' to quit")
 print("Good luck!")
 print()
-playerName = input("Player, what is your name? ")
+try:
+    playerName = input("Player, what is your name? ")
+    if (len(playerName) < 1):
+        playerName = "Anonymous"
+except EOFError as exception:
+    print("Quitting already?")
+    exit(0)
+except Exception as exception:
+    print("Looks like there was a hiccup.  I will call you Anonymous")
 
 # Loop until the player wants to quit
 while True:
 
-    # Begin our guessing loop
-    while correctGuess == 0:
+    try:
+        # Begin our guessing loop
+        while correctGuess == 0:
 
-        # Gather the player's guess
-        try:
-            playerGuess = input("What is your guess? ")
-        except Exception as exception:
-            print(f"An error has occurred.")
-            print(f"Exception: {exception}")
-            print()
-            playerWantsToQuit = True
-
-        # Test the player's guess
-        try:
-            numberOfGuesses, correctGuess = testGuess(playerGuess, numberOfGuesses, someNumber, playerName)
-            if correctGuess == 2:
-                playerWantsToQuit = True
-        except ValueError as exception:
-            playerWantsToQuit = displayErrorMessage("An unknown error has occurred when trying to update the scores.", exception)
-        except Exception as exception:
-            playerWantsToQuit = displayErrorMessage("An unknown error has occurred when trying to update the scores.", exception)
-
-    # If we have thrown an exception, we need to skip this block.
-    #  Otherwise, we can continue.
-    if not playerWantsToQuit:
-
-        # Update the scores
-        try:
-            playerWantsToQuit = updateScores(playerName, numberOfGuesses)
-        except Exception as exception:
-            playerWantsToQuit = displayErrorMessage("An unknown error has occurred when trying to update the scores.", exception)
-
-        # If we have thrown an exception, we need to skip this block.
-        #  Otherwise, we can continue.
-        if playerWantsToQuit != True:
+            # Gather the player's guess
             try:
-                playerWantsToQuit = displayScores()
+                playerGuess = input("What is your guess? ")
+            except EOFError as exception:
+                print("Quitting already?")
+                exit(0)
+
+            except Exception as exception:
+                print(f"An error has occurred.")
+                print(f"Exception: {exception}")
+                print()
+                playerWantsToQuit = True
+
+            # Test the player's guess
+            try:
+                numberOfGuesses, correctGuess = testGuess(playerGuess, numberOfGuesses, someNumber, playerName)
+                if correctGuess == 2:
+                    playerWantsToQuit = True
+            except ValueError as exception:
+                playerWantsToQuit = displayErrorMessage("An unknown error has occurred when trying to update the scores.", exception)
             except Exception as exception:
                 playerWantsToQuit = displayErrorMessage("An unknown error has occurred when trying to update the scores.", exception)
 
-            # Continue on if we do not have an error
-            if playerWantsToQuit != True:
-                print("Would you like to play again?")
-                if input("Enter 'q' to quit anything else to play again. ").lower() == "q":
-                    playerWantsToQuit = True
-                else:
-                    someNumber = randint(1, 100)
-                    numberOfGuesses = 0
-                    playerGuess = ""
-                    correctGuess = 0
-                    playerWantsToQuit = False
+        # If we have thrown an exception, we need to skip this block.
+        #  Otherwise, we can continue.
+        if not playerWantsToQuit:
 
-    # Quitting?  Print out a nice message
-    if playerWantsToQuit:
-        print()
-        print(" GAME OVER ".center(30, "*"))
-        if correctGuess == 2:
-            print(f"The correct number was {someNumber}.")
-        print(f"Thank you, {playerName}, for playing.  I enjoyed it.")
-        print(" GAME OVER ".center(30, "*"))
-        print()
+            # Update the scores
+            try:
+                playerWantsToQuit = updateScores(playerName, numberOfGuesses)
+            except Exception as exception:
+                playerWantsToQuit = displayErrorMessage("An unknown error has occurred when trying to update the scores.", exception)
+
+            # If we have thrown an exception, we need to skip this block.
+            #  Otherwise, we can continue.
+            if playerWantsToQuit != True:
+                try:
+                    playerWantsToQuit = displayScores()
+                except Exception as exception:
+                    playerWantsToQuit = displayErrorMessage("An unknown error has occurred when trying to update the scores.", exception)
+
+                # Continue on if we do not have an error
+                if playerWantsToQuit != True:
+                    print("Would you like to play again?")
+                    if input("Enter 'q' to quit anything else to play again. ").lower() == "q":
+                        playerWantsToQuit = True
+                    else:
+                        someNumber = randint(1, 100)
+                        numberOfGuesses = 0
+                        playerGuess = ""
+                        correctGuess = 0
+                        playerWantsToQuit = False
+
+        # Quitting?  Print out a nice message
+        if playerWantsToQuit:
+            print()
+            print(" GAME OVER ".center(30, "*"))
+            if correctGuess == 2:
+                print(f"The correct number was {someNumber}.")
+            print(f"Thank you, {playerName}, for playing.  I enjoyed it.")
+            print(" GAME OVER ".center(30, "*"))
+            print()
+            break
+    except EOFError as exception:
+        print(f"No hard feelings.  The game is now over.")
+        break
+
+    except Exception as e:
+        print(f"Looks like there was a glitch.  Ending the game nicely.")
         break
